5b712d84b6807f5079290142f52b26841e1f40ba
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-07-01 14:33:31 +0800

    first version

---
+++ src/components/Variant/Variant.tsx
@@ -0,0 +1,36 @@
+import styles from "./variant.module.css"
+
+interface VariantProps {
+  text: string
+  answer: () => void
+  upCorAnsw: () => void
+  correct: boolean
+  stateButton: boolean
+}
+
+function Variant({
+  text,
+  correct,
+  answer,
+  stateButton,
+  upCorAnsw,
+}: VariantProps) {
+  function thisAnswer() {
+    if (correct) {
+      upCorAnsw()
+    }
+    answer()
+  }
+
+  return (
+    <button
+      disabled={!stateButton}
+      className={styles.variant}
+      onClick={thisAnswer}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Variant
