9c6a4451c1f6f771c1cc8d144aa4efdadaa7f2a0
Ko1iya <kolya.fialkovskiy@gmail.com>
2023-07-07 19:11:38 +0800

    pre-release with list

--- src/components/Result/Result.tsx
+++ src/components/Result/Result.tsx
@@ -1,9 +1,22 @@
 import styles from "./result.module.css"
+import questions from "../../data/question"
 
-interface ResultProps {}
+interface ResultProps {
+  correct: boolean
+  index: number
+}
 
-function Result(prop: ResultProps) {
-  return <div className={styles.result}></div>
+function Result({ index, correct }: ResultProps) {
+  return (
+    <div className={styles.result}>
+      <h2 className='title'>{questions[index].title}</h2>
+      <div className='question'>
+        {questions[index].variants.map((variant) => (
+          <p>{variant}</p>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Result
--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,11 +1,19 @@
+import { v4 as uuid } from "uuid"
+
 import styles from "./results.module.css"
-import questions from "../../data/question"
+import Result from "../Result/Result"
 interface ResultsProps {
   arrAnswer: boolean[]
 }
 
-function Results(prop: ResultsProps) {
-  return <div className={styles.results}></div>
+function Results({ arrAnswer }: ResultsProps) {
+  return (
+    <div className={styles.results}>
+      {arrAnswer.map((el, index) => {
+        return <Result index={index} correct={el} key={uuid()} />
+      })}
+    </div>
+  )
 }
 
 export default Results
